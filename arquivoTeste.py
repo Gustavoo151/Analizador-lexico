ad57dcc3315a04cce62b03c3faba3d031180dd0b
Gustavo de oliveira <71619573+Gustavoo151@users.noreply.github.com>
2022-08-19 16:58:26 -0300

    Projeto finalizado

--- arquivoTeste.py
+++ arquivoTeste.py
@@ -4,52 +4,72 @@ tokensInOrder = [] # Resultados dos tokens gerados pelo analisador léxico
 
 identifierTable = [] # Lista para fazer armazenamento do todos os identificadores. Com seus respectivos dados ou não
 
- #Função que recebe um valor e retorna em forma de token
-def find_symb_value_in_table(valueOrSymbol): 
+# Buscando um mach com token usando o parâmetro de entrada
+def searchTokens(valueOrSymbol):
     for searchToken in tokens:  # Buscando um mach com token usando o parâmetro de entrada
     
         if searchToken['lexeme'] == valueOrSymbol:
             tokensInOrder.append(searchToken)
-            return None
+            return True
         
         # Pegando os operadores lógigos relacionais
         elif valueOrSymbol in ['==', '>=', '<=', '!=']:
             if valueOrSymbol == '==' and searchToken['tag'] == 261:
                 tokensInOrder.append(searchToken)
-                return None
+                return True
 
             elif valueOrSymbol == '>=' and searchToken['tag'] == 263:
                 tokensInOrder.append(searchToken)
-                return None
+                return True
             
             elif valueOrSymbol == '<=' and searchToken['tag'] == 267:
                 tokensInOrder.append(searchToken)
-                return None
+                return True
                 
             elif valueOrSymbol == '!=' and searchToken['tag'] == 269:
                 tokensInOrder.append(searchToken)
-                return None
+                return True
 
-            
-    # Condicional para receber valores inteiro/float
+
+# Condicional para receber valores inteiro/float
+def tokensInteger(valueOrSymbol):
     if isinstance(valueOrSymbol, int) or isinstance(valueOrSymbol, float):  # Usando a função isinstance() verifica se um objeto pertence à subclasse especificada
         token = {'tag': 270, 'value': valueOrSymbol}  # Gerando o token da tag 270 que representa um valor int ou float
         tokensInOrder.append(token)
+        return True
+
+
+#FUNÇÃO PRINCIPAL
+# Buscando um mach com token usando o parâmetro de entrada 
+def machToken(valueOrSymbol):
+        for searchSymbol in symbols:        
+            if searchSymbol['tag'] == valueOrSymbol:
+                tokensInOrder.append(searchSymbol)
+                return True
+
+
+ #Função que recebe um valor e retorna em forma de token
+def find_symb_value_in_table(valueOrSymbol): 
+    
+    if searchTokens(valueOrSymbol) == True:
         return None
 
     # Buscando um mach com token usando o parâmetro de entrada 
-    for searchSymbol in symbols:        
-        if searchSymbol['tag'] == valueOrSymbol:
-            tokensInOrder.append(searchSymbol)
-            return None
-
+    elif tokensInteger(valueOrSymbol) == True:
+        return None
+    
+    # Buscando um mach com token usando o parâmetro de entrada 
+    elif machToken(valueOrSymbol) == True:
+        return None
+    
     # Gerando o token da tag 264 que representa qualquer parte do código que o nome pode ser alterado pelo programador    (Após ter passado por todos os condicionais acima só sobraram os identificadores)
     token = {'tag': 264, 'lexeme': f'{valueOrSymbol}'} 
     tokensInOrder.append(token)
     
-    # Verificando se o símbolo já estaá na tabela de símbolos
+    # Verificando se o símbolo já está na tabela de símbolos
     if token not in identifierTable:
         identifierTable.append(token)
+
 
 ######################### CÓDIGO TESTE ####################################################
 '''
@@ -64,8 +84,8 @@ def find_symb_value_in_table(valueOrSymbol):
     if imcNormal == imc;
         meuImc = imc;
 '''
-# LISTA COM PALABRA POR PALAVRA DO CÓDIGO
-casoTesteLista = ['float','altura', '=', 1.75, ';', 'float', 'peso', '=', 80.0, ';', 'int', 'idade', 30, ';', 'float', 'imcNormal', '=', 22.5, ';', 'float', 'meuImc', ';', 'float', 'imc', ';', 'imc', '=', 'peso', '/', '(', 'altura', '*', 'altura', ')', ';', 'if', 'imcNormal', '==', 'imc', ';', 'meuImc', '=', 'imc', ';', '<=', '>=', '!=']
+# LISTA COM PALAVRA POR PALAVRA DO CÓDIGO
+casoTesteLista = ['float','altura', '=', 1.75, ';', 'float', 'peso', '=', 80.0, ';', 'int', 'idade', 30, ';', 'float', 'imcNormal', '=', 22.5, ';', 'float', 'meuImc', ';', 'float', 'imc', ';', 'imc', '=', 'peso', '/', '(', 'altura', '*', 'altura', ')', ';', 'if', 'imcNormal', '==', 'imc', ';', 'meuImc', '=', 'imc', ';',]
 
 ##############################################################################################
 
