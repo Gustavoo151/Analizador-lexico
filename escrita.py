fd7fcc5e38e790db424eca8e35e1209d21052dda
Gustavo de oliveira <71619573+Gustavoo151@users.noreply.github.com>
2022-08-20 23:08:56 -0300

    20/08

---
+++ escrita.py
@@ -0,0 +1,4 @@
+
+
+for i in range(len(casoTesteLista)):
+    print(f'{i+1}: {casoTesteLista[i]}')
--- symb_table_1.0.py
+++ symb_table_1.0.py
@@ -1,33 +1,40 @@
 from tag import tokens, symbols
+import os
 
 tokensInOrder = [] # Resultados dos tokens gerados pelo analisador léxico
 
 identifierTable = [] # Lista para fazer armazenamento do todos os identificadores. Com seus respectivos dados ou não
+
+def arquivoToken(escreveToken):
+    with open('listaTokens.txt', 'a') as token:
+        token.write(f"{escreveToken}\n")
+        token.close()
+
 
 # Buscando um mach com token usando o parâmetro de entrada
 def searchTokens(valueOrSymbol):
     for searchToken in tokens:  # Buscando um mach com token usando o parâmetro de entrada
     
         if searchToken['lexeme'] == valueOrSymbol:
-            tokensInOrder.append(searchToken)
+            arquivoToken(valueOrSymbol)
             return True
         
         # Pegando os operadores lógigos relacionais
         elif valueOrSymbol in ['==', '>=', '<=', '!=']:
             if valueOrSymbol == '==' and searchToken['tag'] == 261:
-                tokensInOrder.append(searchToken)
+                arquivoToken(searchToken)
                 return True
 
             elif valueOrSymbol == '>=' and searchToken['tag'] == 263:
-                tokensInOrder.append(searchToken)
+                arquivoToken(searchToken)
                 return True
             
             elif valueOrSymbol == '<=' and searchToken['tag'] == 267:
-                tokensInOrder.append(searchToken)
+                arquivoToken(searchToken)
                 return True
                 
             elif valueOrSymbol == '!=' and searchToken['tag'] == 269:
-                tokensInOrder.append(searchToken)
+                arquivoToken(searchToken)
                 return True
 
 
@@ -35,7 +42,7 @@ def searchTokens(valueOrSymbol):
 def tokensInteger(valueOrSymbol):
     if isinstance(valueOrSymbol, int) or isinstance(valueOrSymbol, float):  # Usando a função isinstance() verifica se um objeto pertence à subclasse especificada
         token = {'tag': 270, 'value': valueOrSymbol}  # Gerando o token da tag 270 que representa um valor int ou float
-        tokensInOrder.append(token)
+        arquivoToken(token)
         return True
 
 
@@ -43,7 +50,7 @@ def tokensInteger(valueOrSymbol):
 def machToken(valueOrSymbol):
         for searchSymbol in symbols:        
             if searchSymbol['tag'] == valueOrSymbol:
-                tokensInOrder.append(searchSymbol)
+                arquivoToken(searchSymbol)
                 return True
 
 
@@ -64,7 +71,8 @@ def find_symb_value_in_table(valueOrSymbol):
     
     # Gerando o token da tag 264 que representa qualquer parte do código que o nome pode ser alterado pelo programador    (Após ter passado por todos os condicionais acima só sobraram os identificadores)
     token = {'tag': 264, 'lexeme': f'{valueOrSymbol}'} 
-    tokensInOrder.append(token)
+    arquivoToken(token)
+
     
     # Verificando se o símbolo já está na tabela de símbolos
     if token not in identifierTable:
@@ -86,21 +94,21 @@ def find_symb_value_in_table(valueOrSymbol):
 # LISTA COM PALAVRA POR PALAVRA DO CÓDIGO
 casoTesteLista = ['{','int', 'i', ';', 'float', 'v', 'float', 'x', ';', 'float', '[', 100, ']', 'a', ';', 'while', '(', 'true', ')', '{', 'do', 'i', '=', 'i', '+', 1, ';', 'while', '(', 'a', '[', 'i', ']', '<', 'v', ')', ';', 'do', 'j', '=', 'j', '-', 1, ';', 'while', '(', 'a', '[', 'j', ']', '>', 'v', ')', ';', 'if', '(', 'i', '>', '=', 'j', ')', 'break', ';', 'x', '=', 'a', '[', 'i', ']', ';', 'a', '[', 'i', ']', '=', 'a', '[', 'j', ']', ';', 'a', '[', 'j', ']', '=', 'x', ';', '}','}']
 
-##############################################################################################ALO
+##############################################################################################
 
 # Usando List Comprehensions para passar valor por valor na função principal
 [find_symb_value_in_table(dados) for dados in casoTesteLista]
 
-print("-="*25)
-print("\tTOKENS EM ORDEM")
-print("-="*25)
-# Usando List Comprehensions para printar os tokens em ordem
-[print(f'Token: {tokens}') for tokens in tokensInOrder]
-print(""*20)
-print('')
-print("-="*20)
-print('\tTABELA DE SÍMBOLOS')
-print("-="*20)
+# print("-="*25)
+# print("\tTOKENS EM ORDEM")
+# print("-="*25)
+# # Usando List Comprehensions para printar os tokens em ordem
+# [print(f'Token: {tokens}') for tokens in tokensInOrder]
+# print(""*20)
+# print('')
+# print("-="*20)
+# print('\tTABELA DE SÍMBOLOS')
+# print("-="*20)
 # Usando List Comprehensions para printar a tabela se símbolos
 [print(tokens) for tokens in identifierTable]
 print("-="*20)
